c60cd75bfbe70f0b7882e8a5e380ca5a71d062af
Yu Myungjae <u4bi@letsee.io>
2017-07-28 18:46:00 +0900

    email.js comment
    - http://emailregex.com

--- src/modules/Email.js
+++ src/modules/Email.js
@@ -1,2 +1,2 @@
-// http://emailregex.com/
+// source http://emailregex.com/
 export const email = (value) => /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(value);
