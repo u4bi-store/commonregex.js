9bdcf17ca6341881c01b9dfc35d781d53acacfb3
Yu Myungjae <u4bi@letsee.io>
2017-07-28 18:57:12 +0900

    ip4.js regex render

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ export { a } from './modules/A';
 export { email } from './modules/Email';
 export { hangul } from './modules/Hangul';
 export { phonekr } from './modules/PhoneKR';
+export { ip4 } from './modules/Ip4';
